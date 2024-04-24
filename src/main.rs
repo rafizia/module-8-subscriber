7bb6737ab5d5957c87544f03614692f1f522ae83
Muhammad Rafi Zia Ulhaq <mohrafiziaulhaq@gmail.com>
2024-04-24 13:52:22 +0700

    fix typo

--- src/main.rs
+++ src/main.rs
@@ -28,8 +28,7 @@ UserCreatedHandler {
  
 fn main() { 
     let listener = 
-CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:567
- 2".to_owned()).unwrap(); 
+CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:5672".to_owned()).unwrap(); 
     _ = listener.listen("user_created".to_owned(), 
 UserCreatedHandler{}, crosstown_bus::QueueProperties { 
 auto_delete: false, durable: false, use_dead_letter: true }); 
