60c5325e7dccc5dffc0f93ed9b8cd1dc884f2cee
Muhammad Rafi Zia Ulhaq <mohrafiziaulhaq@gmail.com>
2024-04-24 14:17:51 +0700

    Simulation slow subscriber

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# module-8-subscriber" 
+
+![alt text](https://github.com/rafizia/module-8-subscriber/blob/master/image/RabbitMQ-Slow-1.png?raw=true)
--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ UserCreatedHandler {
         let ten_millis = time::Duration::from_millis(1000); 
         let now = time::Instant::now(); 
          
-        // thread::sleep(ten_millis); 
+         thread::sleep(ten_millis); 
       
         println!("2206814551 [129500004y]. Message 
         received: {:?}", message); 
